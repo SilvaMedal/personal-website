dc1b23e7f6bb36aba3803aa1e266638ab1f8a801
Anthony Silva <budsilva04@gmail.com>
2023-09-04 15:29:02 -0400

    Edit css, add frame for about section

--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,19 @@ function App() {
           <span className="revealing-text delay-2">Medal</span>
           <span className="revealing-text delay-3">Productions</span>
         </h1>
-        <p className="">
-          Click on the <code>React</code> logo to learn more
+        <h3 className="revealing-text delay-4">About Me:</h3>
+        <p className="revealing-text delay-4">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Lectus
+          proin nibh nisl condimentum id venenatis a condimentum. Lorem donec
+          massa sapien faucibus et molestie ac. Pellentesque habitant morbi
+          tristique senectus. Id venenatis a condimentum vitae. Sed risus
+          pretium quam vulputate dignissim. Mattis ullamcorper velit sed
+          ullamcorper morbi tincidunt ornare massa. Aliquet nec ullamcorper sit
+          amet risus nullam eget felis eget. Orci nulla pellentesque dignissim
+          enim sit amet venenatis. Viverra aliquet eget sit amet tellus. Enim
+          blandit volutpat maecenas volutpat blandit aliquam etiam. Congue eu
+          consequat ac felis donec et odio pellentesque.{" "}
         </p>
       </main>
     </>
