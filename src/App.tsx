1c39a18747d1ac66e9618e6caa52684a4adcd9eb
Anthony Silva <budsilva04@gmail.com>
2023-09-25 22:00:22 -0400

    Edit some css styling, fix linkedin logo

--- src/App.tsx
+++ src/App.tsx
@@ -18,20 +18,21 @@ function App() {
           <section id="about" className="section">
             <h3>About Me:</h3>
             <p>
-              Hi, I'm Anthony Silva, a freelance web developer driven by an
-              insatiable thirst for knowledge. With over 16 years of experience
-              in the dynamic world of hospitality, primarily in restaurant
-              management, I transitioned into the realm of web development 2.5
-              years ago.{" "}
+              Hi, I'm Anthony Silva, a freelance web developer driven by a
+              passion for knowledge. With over 16 years of experience in the
+              world of hospitality, primarily in restaurant management, I
+              transitioned into the realm of web development 2.5 years ago. My
+              journey has cemented my dedication to personal growth and my
+              commitment to fostering tech innovation.{" "}
             </p>
+            <p>[***INCLUDE MORE ABOUT ME PERSONALLY HERE***]</p>
             <p>
               Currently, I lead the front-end development team at CD Techies, a
               non-profit organization that unites both budding tech enthusiasts
               and seasoned mentors. CD Techies is on a mission to fulfill the
               technological needs of other non-profit organizations while
               serving as an invaluable educational resource for newcomers in the
-              tech industry. My journey has been a testament to my dedication to
-              personal growth and my commitment to fostering tech innovation.
+              tech industry.
             </p>
           </section>
           <section id="portfolio" className="section">
